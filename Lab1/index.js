037b98c455a315a84afb3a163177c70ff106effe
Kajciuch <kajat224@gmail.com>
2025-10-06 11:18:14 +0200

    update

--- Lab1/index.js
+++ Lab1/index.js
@@ -1,9 +1,14 @@
-const fs = require("fs"); 
+const http = require("http"); 
 
-console.log("Początek skryptu."); 
-fs.readFile('package.json', function (err, data) { 
-    if (err) return 
-    console.error(err); 
-    console.log(data.toString()); 
+const hostname = '127.0.0.1'; 
+const port = 8888;
+
+const server = http.createServer(function(request, response) { 
+    response.statusCode = 200; 
+    response.setHeader('Content-Type', 'text/plain'); 
+    response.end('Witaj, z serwera node.js !\n'); 
 }); 
-console.log("Koniec skryptu."); 
+
+server.listen(port, hostname, () => { 
+    console.log(`Server running at http://${hostname}:${port}/`); 
+});   
