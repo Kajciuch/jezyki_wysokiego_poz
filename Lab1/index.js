acfaf5c0eadb1df8baea3e7188266960c1a02da4
Kajciuch <kajat224@gmail.com>
2025-10-06 11:29:35 +0200

    update

--- Lab1/index.js
+++ Lab1/index.js
@@ -1,4 +1,7 @@
-const http = require("http"); 
+const http = require("http");
+const path = require("path");
+const util = require("util");
+const v8 = require("v8"); 
 
 const hostname = '127.0.0.1'; 
 const port = 8888;
@@ -10,5 +13,8 @@ const server = http.createServer(function(request, response) {
 }); 
 
 server.listen(port, hostname, () => { 
+    util.log(v8.getHeapStatistics()); 
+    console.log(path.basename(__filename)); 
+    util.log(path.join(__dirname, 'uploads','images')); 
     console.log(`Server running at http://${hostname}:${port}/`); 
 });   
