af7b5cb2a765880a9bca12e6721a239d2799f261
Kajciuch <kajat224@gmail.com>
2025-11-03 10:57:30 +0100

    update

--- Lab5/Lab5ReactDev/my-app/src/App.js
+++ Lab5/Lab5ReactDev/my-app/src/App.js
@@ -1,22 +1,11 @@
-import './App.css';
-import './Hello';
-import './ToggleDetails';
-import './ScoreDisplay';
-import './UserList';
-import './TaskList';
+import ScoreDisplay from './ScoreDisplay'; 
+import TimerCounter from './TimerCounter';
+import  ToggleDetails from'./ToggleDetails';
+import TaskList from './TaskList';
 
+function App() { return (
+     <div> 
+        </div> ); 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-        </p>
-        <a>
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+} 
+export default App; 
